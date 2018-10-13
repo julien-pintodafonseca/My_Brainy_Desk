e438efa257d9ed0f87f39c135f694e6d9c8321b8
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-10-13 11:25:57 +0200

    Ajout des principaux composants

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'accueil',
+        pathMatch: 'full'
+    },
+    {
+        path: 'accueil',
+        component: HomeComponent
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+    providers: []
+})
+
+export class AppRoutingModule {
+
+}
