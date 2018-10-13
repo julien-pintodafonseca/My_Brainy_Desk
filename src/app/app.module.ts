be3d57f6858637b3201558da7163667b6a18a579
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-10-13 12:00:32 +0200

    Màj structure rooter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { BddCreateComponent } from './components/bdd-create/bdd-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PageNotFoundComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    BddCreateComponent
   ],
   imports: [
     BrowserModule,
