935d98307bcfd36abfac8ab57044e578fe2735b7
SkydeFR <SkydeFR@gmail.com>
2018-10-13 16:20:24 +0200

    component newPartenaire

---
+++ src/app/components/new-partenaires/new-partenaires.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-partenaires',
+  templateUrl: './new-partenaires.component.html',
+  styleUrls: ['./new-partenaires.component.css']
+})
+export class NewPartenairesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
