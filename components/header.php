899499b993540e45beb63804c1eb8ba277fb15c4
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-10-13 22:35:09 +0200

    Màj

---
+++ components/header.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="container-fluid title pt-4 pb-4" style="min-height:234px;">
+
+        <div class="row" style="background-color:white;border-radius:100px;width:567px;height:133px;padding:10px;text-align:center;align-items:center;">
+            <div class="col-4">
+              <img src="media/image/header/logo.png" class="img-responsive" width="100px" height="100px">
+            </div>
+            <div class="col-8">
+                <h1>
+                    DeskPlace
+                </h1>
+            </div>
+        </div>
+
+    </div>
+
+    <div style="background:radial-gradient(circle, rgba(0,172,255,1) 0%, rgba(124,0,255,1) 100%);height:5px;vertical-align:middle;"></div>
+
+    <?php include('navigation.php'); ?>
+
+</header>
---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+    <title>Titre</title>
+    <link rel='stylesheet' href='css/style.css'>
+    <link rel="icon" href="icon.ico">
+</head>
+
+<body>
+    <?php include("components/header.php"); ?>
+
+
+
+
+
+    <div class="container">
+    
+    
+        <h1>Titre 1</h1>
+        <h2>Titre 2</h2>
+        <h3>Titre 3</h3>
+        <h4>Titre 4</h4>
+        <p>Paragraphe</p>
+        
+    </div>
+
+
+
+
+
+
+</body>
+
+
+</html>
