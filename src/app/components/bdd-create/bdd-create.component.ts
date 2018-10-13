b938cec5e68b84814f81c00f36f67a1823ab1e01
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-10-13 12:00:20 +0200

    Added bdd_create component

---
+++ src/app/components/bdd-create/bdd-create.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bdd-create',
+  templateUrl: './bdd-create.component.html',
+  styleUrls: ['./bdd-create.component.css']
+})
+export class BddCreateComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    
+  }
+
+}
