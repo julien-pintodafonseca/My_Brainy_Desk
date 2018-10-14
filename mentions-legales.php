67ba721e60874633b349aa1a0e25f25567af4c12
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-10-14 14:27:06 +0200

    Update mentions-legales.php

--- mentions-legales.php
+++ mentions-legales.php
@@ -19,7 +19,23 @@
             <div class="row">
                 <div class="col">
                     <h1>Mentions Légales</h1>
-					<p style="text-align:center"><a href="mailto:contact@brainytech.fr">contact@brainytech.fr</a></p>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="row" style="align-items:center;">
+                <div class="col-5 text-center">
+                    <img src="media/image/mentions-legales.jpg" alt="Mentions légales" style="max-height:500px;width:auto;">
+                </div>
+                <div class="col">
+                    <h2 class="mb-3">Editeur</h2>
+                    <p class="mb-4"><b>Nom</b><br />San Antonio</p>
+                    <p class="mb-4"><b>Adresse mail</b><br />contact@editeur.fr</p>
+                    <br />
+                    <h2 class="mb-3">Hébergement</h2>
+                    <p class="mb-4"><b>Adresse</b><br /><a href="#">MyHebergeur</a><br /> 9 rue de l'angular<br />52000, Lyon</p>
+                    <p class="mb-4"><b>Téléphone</b><br />+33 6 72 82 69 42</p>
                 </div>
             </div>
         </section>
