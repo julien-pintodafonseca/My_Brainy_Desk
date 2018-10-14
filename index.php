c63e968a11ab8ed5009fe512a7a4ac8e8b1699b7
Inkan <inkandev@gmail.com>
2018-10-14 11:13:56 +0200

    Màj de l'index

--- index.php
+++ index.php
@@ -26,25 +26,15 @@
                     <img src="media/image/accueil/environnement.jpg" alt="" style="max-height:600px;width:auto;">
                 </div>
                 <div class="col">
-                    <h2 class="mb-2">Multipliez votre productivité</h2>
-                    <p>
+                    <h2 class="mb-3">Multipliez votre productivité</h2>
+                    <p class="mb-2">
                         Vous êtes un entrepreneur indépendant ou une jeune startup qui ne dispose pas des moyens d'investir un bureau ? Retrouvez ponctuellement un environnement de travail inspirant et pensé pour l'échange à l'aide de MBDesk et augmentez votre productivité de manière exponentielle.
                     </p>
-                    <a class="highlight" href="registration.php">Créer un compte utilisateur</a>
+                    <a class="highlight" href="inscription.php">Créer un compte utilisateur</a>
                 </div>
             </div>
         </section>
 
-        <section>
-            <div class="row">
-                <div class="col">
-                    <h2 class="text-center mb-2">Par tous, pour tous</h2>
-                    <p class="text-center">
-                        MBDesk relaie les annonces de ses nombreux partenaires à travers la France pour vous proposer un choix de location vaste. Vous trouverez des bureaux, salles de réunion, de formation ou espaces de co-working.......
-                    </p>
-                </div>
-            </div>
-        </section>
 
         <?php include("components/footer.php") ?>
 
