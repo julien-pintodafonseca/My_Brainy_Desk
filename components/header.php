3ddf8697e5006b1da52a36d332a5a22d3e8ea99f
Inkan <inkandev@gmail.com>
2018-10-14 16:47:09 +0200

    Update components/header.php

--- components/header.php
+++ components/header.php
@@ -1,8 +1,6 @@
 <header>
     <div class="container-fluid pt-4 pb-4">
-
-
-        <div class="row text-center ml-3 mt-4 mb-4" style="background-color:white;border-radius:100px;padding:10px;align-items:center;width:567px;">
+        <div class="row shadowbox text-center ml-3 mt-4 mb-4" style="background-color:white;border-radius:100px;padding:10px;align-items:center;width:567px;">
             <div class="col-2">
                 <img src="media/image/header/logo.png" class="img-responsive" width="100px" height="100px">
             </div>
@@ -10,15 +8,8 @@
                 <h1>//MBDesk::</h1>
             </div>
         </div>
-        <div class="col">
-
-
-        </div>
-
-
-
     </div>
-
+    
     <div style="background:radial-gradient(circle, rgba(0,172,255,1) 0%, rgba(124,0,255,1) 100%);height:5px;vertical-align:middle;"></div>
 
     <?php include('navigation.php'); ?>
