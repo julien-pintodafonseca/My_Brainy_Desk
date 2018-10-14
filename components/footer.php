88a30d7df26c32013e07464197abc57038d778d7
Inkan <inkandev@gmail.com>
2018-10-14 11:10:13 +0200

    Màj du footer

--- components/footer.php
+++ components/footer.php
@@ -1,35 +1,27 @@
 <footer>
 
     <div class="row">
-        <div class="col-md-3" data-aos="fade-up" data-aos-delay="100" data-aos-once="true">
+        <div class="col-md-4" data-aos="fade-up" data-aos-delay="100" data-aos-once="true">
             <ul>
-                <li class="title"><a href="http://www.inkandev.fr/mybrainydesk">MBDesk</a></li>
+                <li class="title"><a href="http://www.inkandev.fr/mybrainydesk/">MBDesk</a></li>
                 <li><a href="a-propos.php">À propos</a></li>
                 <li><a href="contact.php">Contact</a></li>
                 <li><a href="mentions-legales.php">Mentions légales</a></li>
             </ul>
         </div>
-        <div class="col-md-3" data-aos="fade-up" data-aos-delay="200" data-aos-once="true">
-            <ul>
-                <li class="title"><a href="projets.php">Projets</a></li>
-                <li><a href="#">Site personnel</a></li>
-                <li><a href="#">Spaceships</a></li>
-                <li><a href="#">Plus...</a></li>
-            </ul>
-        </div>
-        <div class="col-md-3" data-aos="fade-up" data-aos-delay="400" data-aos-once="true">
+        <div class="col-md-4" data-aos="fade-up" data-aos-delay="400" data-aos-once="true">
             <ul>
                 <li class="title"><a href="contact.php">Coordonnées</a></li>
-                <li><a href="contact.php">06 51 70 53 41</a></li>
-                <li><a href="contact.php">inkandev@gmail.com</a></li>
+                <li><a href="contact.php">06 12 34 56 78</a></li>
+                <li><a href="contact.php">contact@mbdesk.com</a></li>
             </ul>
         </div>
-        <div class="col-md-3" data-aos="fade-up" data-aos-delay="500" data-aos-once="true">
+        <div class="col-md-4" data-aos="fade-up" data-aos-delay="500" data-aos-once="true">
             <ul>
                 <li class="title"><a href="">Réseaux</a></li>
-                <li><a href="https://github.com/InkanDev" target="_blank">GitHub</a></li>
-                <li><a href="https://www.linkedin.com/in/angepagel/" target="_blank">LinkedIn</a></li>
-                <li><a href="#">Google+</a></li>
+                <li><a href="#" target="_blank">LinkedIn</a></li>
+                <li><a href="#" target="_blank">Twitter</a></li>
+                <li><a href="#" target="_blank">Facebook</a></li>
             </ul>
         </div>
     </div>
