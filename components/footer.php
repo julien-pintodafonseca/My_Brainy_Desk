93c2db267820c236aa59ce7a064dee6b6f6ddab4
Kilian Da Silva Naudin <42610725+Argawaenn@users.noreply.github.com>
2018-10-14 13:25:40 +0200

    Update components/footer.php

--- components/footer.php
+++ components/footer.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-md-4" data-aos="fade-up" data-aos-delay="100" data-aos-once="true">
             <ul>
-                <li class="title"><a href="http://www.inkandev.fr/mybrainydesk/">MBDesk</a></li>
+                <li class="title"><a href="https://www.inkandev.fr/mybrainydesk/">MBDesk</a></li>
                 <li><a href="a-propos.php">À propos</a></li>
                 <li><a href="contact.php">Contact</a></li>
                 <li><a href="mentions-legales.php">Mentions légales</a></li>
@@ -12,8 +12,8 @@
         <div class="col-md-4" data-aos="fade-up" data-aos-delay="400" data-aos-once="true">
             <ul>
                 <li class="title"><a href="contact.php">Coordonnées</a></li>
-                <li><a href="contact.php">06 12 34 56 78</a></li>
-                <li><a href="contact.php">contact@mbdesk.com</a></li>
+                <li><a href="contact.php">06 4 27 91 41</a></li>
+                <li><a href="contact.php">contact@brainytech.fr</a></li>
             </ul>
         </div>
         <div class="col-md-4" data-aos="fade-up" data-aos-delay="500" data-aos-once="true">
